1c3e4053f120e9a6885bbe197a426434532c2ecd
Mahan Moulaei <75180223+mahanmoulaei@users.noreply.github.com>
2023-06-11 23:33:32 -0400

    tweak(config): url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,11 +11,10 @@ const config = {
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://esx-overextended.github.io",
+  url: "https://github.com",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/",
-  trailingSlash: false,
+  baseUrl: "/esx-overextended.github.io/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
